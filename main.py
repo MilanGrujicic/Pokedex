f46ec7c15fe9c370790de56f595d0b08d1eb7372
Milan <milangru@gmail.com>
2024-04-26 21:11:44 +0200

    Added feature that displays pokemon info.

--- main.py
+++ main.py
@@ -20,19 +20,23 @@ class Pokedex:
         self.user_input = Text(bg="#084035", fg="white", height=1, width=31, borderwidth=0)
         self.user_input.place(x=480, y=235)
 
+        self.information_display = Frame(self.window, bg="black", width=130, height=150)
+        self.information_display.place(x=190, y=220)
+
         self.close_button = Button(text="Search", width=9, height=2, bg="#5fc480", highlightbackground="black", border="2", font=("Helvetica", 17, "bold"),command=self.search_pokemon).place(x=100,y=509)
 
         self.window.mainloop()
 
     def search_pokemon(self):
-        pokemon_info = self.sprite_url = self.get_pokemon_info()
-        threading.Thread(target=self.getImageFromURL, args=(pokemon_info[-1], self)).start()
-        self.imagelab = Label(self.window, text="Loading image from internet ...", width=25, height=5, bg="black", fg="white")
+        pokemon_information = self.get_pokemon_information()
+        threading.Thread(target=self.getImageFromURL, args=(pokemon_information[-1], self)).start()
+        self.imagelab = Label(self.window, text="Loading Pokemon data ...", width=25, height=5, bg="black", fg="white")
         self.imagelab.place(x=90, y=250)
         self.window.bind("<<ImageLoaded>>", self.on_image_loaded)
+        self.display_pokemon_information(pokemon_information)
 
-    def get_pokemon_info(self):
-        pokemon_info = list()
+    def get_pokemon_information(self):
+        pokemon_information = list()
         pokemon_name = self.user_input.get(1.0, END).strip()  # Get and strip the input text
         if not pokemon_name:
             print("Please enter a Pokémon name.")
@@ -43,14 +47,12 @@ class Pokedex:
 
         if response.status_code == 200:
             data = response.json()
-            pokemon_info.append(pokemon_name)
-            pokemon_info.append(data["id"])
-            type = data["types"][0]["type"]["name"]
-            type = type.title()
-            pokemon_info.append(type)
-            pokemon_info.append(data["abilities"][1]["ability"]["name"])
-            pokemon_info.append(data["sprites"]["front_default"])
-            return pokemon_info
+            pokemon_information.append(data["id"])
+            pokemon_information.append(pokemon_name)
+            pokemon_information.append(data["types"][0]["type"]["name"])
+            pokemon_information.append(data["abilities"][1]["ability"]["name"])
+            pokemon_information.append(data["sprites"]["front_default"])
+            return pokemon_information
         else:
             print(f"Error fetching Pokémon data: {response.status_code}")
             return None
@@ -67,6 +69,32 @@ class Pokedex:
     def on_image_loaded(self, event):
         self.imagelab.config(image=self.image, width=self.image.width(), height=self.image.height())
 
+    def display_pokemon_information(self, list_with_pokemon_information):
+        print("display_pokemon_information")
+
+        self.id = Label(self.information_display, text="ID:", bg="black", fg="white")
+        self.id.grid(column=0, row=0, pady=5, sticky="e")
+
+        self.pokemon_id = Label(self.information_display, text=list_with_pokemon_information[0], bg="black", fg="white")
+        self.pokemon_id.grid(column=1, row=0, pady=5, sticky="w")
+
+        self.name = Label(self.information_display, text="Name:", bg="black", fg="white")
+        self.name.grid(column=0, row=1, pady=5, sticky="e")
+
+        self.pokemon_name = Label(self.information_display, text=list_with_pokemon_information[1].title(), bg="black", fg="white",)
+        self.pokemon_name.grid(column=1, row=1, pady=5, sticky="w")
+
+        self.type = Label(self.information_display, text="Type:", bg="black", fg="white")
+        self.type.grid(column=0, row=2, pady=5, sticky="e")
+
+        self.pokemon_type = Label(self.information_display, text=list_with_pokemon_information[2].title(), bg="black", fg="white")
+        self.pokemon_type.grid(column=1, row=2, pady=5, sticky="w")
+
+        self.ability = Label(self.information_display, text="Ability:", bg="black", fg="white")
+        self.ability.grid(column=0, row=3, pady=5, sticky="e")
+
+        self.pokemon_type = Label(self.information_display, text=list_with_pokemon_information[3].title(), bg="black", fg="white")
+        self.pokemon_type.grid(column=1, row=3, pady=5, sticky="w")
 
 if __name__ == "__main__":
     app = Pokedex()
